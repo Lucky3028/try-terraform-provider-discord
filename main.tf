6962748fbafb6ea1440b06585b8bd1add588659c
Lucky <Lucky3028@users.noreply.github.com>
2022-10-31 09:40:25 +0900

    feat: add main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+tterraform {
+  required_providers {
+    discord = {
+      source  = "Lucky3028/discord"
+      version = "1.1.0"
+    }
+  }
+
+  cloud {
+    organization = "lucky3028"
+
+    workspaces {
+      name = "Kagerou"
+    }
+  }
+}
+
+provider "discord" {
+  token = var.discord_bot_token
+}
